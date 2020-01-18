8b35df1a4a80d5295d7213465d7d2e18e88718fe
Sam Moore <swmoore84@gmail.com>
2020-01-18 11:45:31 +0900

    changed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   resources :cocktails, only: [:new, :create, :index, :show] do
     resources :doses, only: [:new, :create, :destroy], shallow: true
   end
