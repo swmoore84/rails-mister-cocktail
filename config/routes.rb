82a1dd4749920251b48f72013986d6da1499dc31
Sam Moore <swmoore84@gmail.com>
2020-01-18 11:17:02 +0900

    added styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:new, :create, :index, :show] do
+    resources :doses, only: [:new, :create, :destroy], shallow: true
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+
+open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list') do |ingredients|
+  ingredients.read.each_line do |ingredient|
+    items = JSON.parse(ingredient)['drinks']
+    items.each do |item|
+      name = item['strIngredient1']
+      Ingredient.create(name: name)
+    end
+  end
+end
